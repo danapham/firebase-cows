e31fbb3b88c544c435f5e532f53a09a375c8708e
Dana Pham <danapham12@gmail.com>
2020-09-19 13:58:08 -0500

    took out console warns and comments

--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -7,18 +7,13 @@ const getUser = (userObj) => {
   axios.get(`${baseURL}/farmers.json?orderBy="uid"&equalTo="${userObj.uid}"`)
     .then((resp) => {
       if (Object.values(resp.data).length === 0) {
-        // Post Object to Farmers
-        // {
-        //   image:
-        //   uid:
-        //   displayName:
-        //   email:
-        //   lastSignInTime:
-        // }
-
-        // use axios.post
-        // pass it the url${baseURL}/farmers.json
-        // past it an object
+        axios.post(`${baseURL}/farmers.json`, {
+          image: `${userObj.photoURL}`,
+          uid: `${userObj.uid}`,
+          displayName: `${userObj.displayName}`,
+          email: `${userObj.email}`,
+          lastSignInTime: `${userObj.metadata.lastSignInTime}`
+        });
       } else {
         console.warn('User Exists');
       }
