eb44c4b41e073a409adfbb65a3e6b0e183f664d3
Dana Pham <danapham12@gmail.com>
2020-09-19 10:42:00 -0500

    saving authentication

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,16 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import authData from './helpers/data/authData';
+import auth from './components/auth/auth';
+import myNavBar from './components/myNavBar/myNavBar';
+
 import '../styles/main.scss';
 
 const init = () => {
-  console.warn('HELLO!');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavBar.logoutEvent();
 };
 
 init();
